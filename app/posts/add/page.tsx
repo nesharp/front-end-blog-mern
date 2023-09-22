05b482ac4233e8d6f7b9348d59ced3404dfdb171
nesharp <nesterukmisha27@gmail.com>
2023-09-22 11:05:29 +0300

    metadata error fixed

--- app/posts/add/page.tsx
+++ app/posts/add/page.tsx
@@ -2,10 +2,7 @@
 import { useAuthRedirect } from '@/src/hooks/useAuthRedirect'
 import { AddPostPage } from '@/src/components/screens/AddPostPage/AddPostPage'
 import { Metadata } from 'next'
-export const metadata: Metadata = {
-	title: 'Add Post',
-	description: 'Add a post to the blog'
-}
+
 export default () => {
 	useAuthRedirect()
 	return <AddPostPage />
